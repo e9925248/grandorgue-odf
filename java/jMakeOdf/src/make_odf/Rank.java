96595b91929996ff156ca2daca60907a708dfa77
Marcin Listkowski <MarcinListkowski@users.noreply.github.com>
2014-08-16 23:01:08 +0200

    merge copied code in Rank and Stop

--- java/jMakeOdf/src/make_odf/Rank.java
+++ java/jMakeOdf/src/make_odf/Rank.java
@@ -25,7 +25,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rank {
+public class Rank implements IPipeSet {
 	String name;
 	int firstMidiNoteNumber;
 	int numberOfLogicalPipes;
@@ -67,91 +67,9 @@ public class Rank {
 	int readPipesLine(Tokenizer tok, boolean loadOneSamplePerPipe) {
 		List<String> stringParts = tok.readAndSplitLine();
 		int pipesThisLine = Tokenizer.convertToInt(stringParts.get(0));
-		String loadString = stringParts.get(1);
-		switch (loadString.charAt(0)) {
-		case 'L':
-			String pathToSearch = stringParts.get(2);
-			String loadAttRel = stringParts.get(3);
-			boolean pipePercussive = Tokenizer.convertToBoolean(stringParts
-					.get(4));
-			int startMidiNote = Tokenizer.convertToInt(loadString.substring(1,
-					loadString.length()));
-			for (int j = 0; j < pipesThisLine; j++) {
-				Pipe p = Pipe.loadSamples(startMidiNote + j, loadString,
-						pathToSearch, loadAttRel, pipePercussive,
-						loadOneSamplePerPipe);
-				// FIXME isPercussive - is it omitted here on purpose? compare
-				// to next calls of setBasicAttributes
-				setBasicAttributes(p);
-				m_Pipes.add(p);
-			}
-			break;
-		case 'R':
-			int startPipe = Tokenizer.convertToInt(loadString.substring(1,
-					loadString.length()));
-			String keybCode = stringParts.get(2);
-			int stop = Tokenizer.convertToInt(stringParts.get(3));
-			for (int j = 0; j < pipesThisLine; j++) {
-				Pipe p = Pipe.createReference(startPipe + j, keybCode, stop);
-				p.isPercussive = isPercussive;
-				setBasicAttributes(p);
-				m_Pipes.add(p);
-			}
-			break;
-		case 'C':
-			int startCopy = Tokenizer.convertToInt(loadString.substring(1,
-					loadString.length())) - 1;
-			float pitchChange = Tokenizer.convertToFloat(stringParts.get(2));
-			for (int j = 0; j < pipesThisLine; j++) {
-				Pipe source = m_Pipes.get(startCopy + j);
-				Pipe p = new Pipe(source);
-				p.pitchTuning = pitchChange;
-				m_Pipes.add(p);
-			}
-			break;
-		case 'M':
-			int nextIndex = 2;
-			for (int k = 0; k < pipesThisLine; k++) {
-				Pipe p = new Pipe();
-				Attack a = new Attack();
-				a.fileName = stringParts.get(nextIndex);
-				nextIndex++;
-				if (Tokenizer.convertToBooleanInverted(stringParts
-						.get(nextIndex))) {
-					a.loadRelease = false;
-					a.attackVelocity = 0;
-					a.maxKeyPressTime = -1;
-					nextIndex++;
-					p.attacks.add(a);
-					Release r = new Release();
-					r.fileName = stringParts.get(nextIndex);
-					nextIndex++;
-					p.releases.add(r);
-				} else {
-					a.loadRelease = true;
-					a.attackVelocity = 0;
-					a.maxKeyPressTime = -1;
-					nextIndex++;
-					p.attacks.add(a);
-				}
-				p.isPercussive = isPercussive;
-				setBasicAttributes(p);
-				m_Pipes.add(p);
-			}
-			break;
-		default:
-			throw new TextFileParserException("ERROR: Load method "
-					+ loadString + " for stop " + name + " is invalid!");
-		}
+		PipeSetUtil.readPipeSet(this, loadOneSamplePerPipe, stringParts,
+				pipesThisLine);
 		return pipesThisLine;
-	}
-
-	public void setBasicAttributes(Pipe p) {
-		p.amplitudeLevel = amplitudeLevel;
-		p.harmonicNumber = harmonicNumber;
-		p.pitchTuning = pitchTuning;
-		p.pitchCorrection = pitchCorrection;
-		p.windchestGroup = m_windchestGroup;
 	}
 
 	public void read(Tokenizer tok, boolean loadOneSamplePerPipe) {
@@ -188,5 +106,39 @@ public class Rank {
 		outfile.println("PitchTuning=" + pitchTuning);
 		outfile.println("PitchCorrection=" + pitchCorrection);
 		outfile.println("HarmonicNumber=" + harmonicNumber);
+	}
+
+	@Override
+	public void addPipe(Pipe p) {
+		m_Pipes.add(p);
+	}
+
+	@Override
+	public String getKindName() {
+		return "rank";
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Pipe getPipe(int index) {
+		return m_Pipes.get(index);
+	}
+
+	@Override
+	public boolean isPercussive() {
+		return isPercussive;
+	}
+
+	@Override
+	public void setBasicAttributes(Pipe p) {
+		p.amplitudeLevel = amplitudeLevel;
+		p.harmonicNumber = harmonicNumber;
+		p.pitchTuning = pitchTuning;
+		p.pitchCorrection = pitchCorrection;
+		p.windchestGroup = m_windchestGroup;
 	}
 }
